43e6a3f17bccf1bf42ad1bb98868d66620670354
Joana <joanajanefdez@gmail.com>
2022-08-31 23:22:25 +0200

    ex 3 done

---
+++ ex3/js/code.js
@@ -0,0 +1,6 @@
+let meses = ["Enero","Febrero","Marzo","Abril","Mayo","Junio",
+    "Agosto","Septiembre","Octubre","Noviembre","Diciembre"];
+
+for(let i=0;i<meses.length;i++){
+    console.log(meses[i])
+}
